5f46bf225d7eb0011f65ac75355b5382d4007681
llorenzinho <lorenzo.dagostino.ref@gmail.com>
2024-01-19 00:28:33 +0100

    removed useless import

--- lib/features/cities/utils/table_data_source.dart
+++ lib/features/cities/utils/table_data_source.dart
@@ -1,5 +1,3 @@
-import 'dart:js';
-
 import 'package:flutter/material.dart';
 import 'package:sinergia/features/cities/domain/entities/city_entity.dart';
 import 'package:sinergia/features/documents/presentation/pages/test_page.dart';
